b6c63624ea77a54b03e1336bd70e62108e788431
Tony Faijue <tony.faijue@ndus.edu>
2025-08-15 14:59:49 -0500

    Image Preview & Upload Image Components

--- frontend/EchoText/src/app/echo-text/echo-text.component.ts
+++ frontend/EchoText/src/app/echo-text/echo-text.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { WindowFrameComponent } from '../window-frame/window-frame.component';
+import { ImagePreviewComponent } from '../image-preview/image-preview.component';
+import { UploadImageComponent } from '../upload-image/upload-image.component';
 
 @Component({
   selector: 'app-echo-text',
-  imports: [WindowFrameComponent],
+  imports: [WindowFrameComponent, ImagePreviewComponent, UploadImageComponent],
   templateUrl: './echo-text.component.html',
   styleUrl: './echo-text.component.css'
 })
